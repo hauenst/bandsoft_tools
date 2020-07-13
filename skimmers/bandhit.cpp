b16964522422a0e928812532cef4bb3058895cc5
Efrain Segarra <efrainsegarra@efrains-mbp.lan>
2020-07-13 12:52:02 -0400

    * Fixed class issue with root

---
+++ skimmers/bandhit.cpp
@@ -0,0 +1,41 @@
+#include "bandhit.h"
+
+bandhit::bandhit(void){}
+
+void bandhit::Clear(){
+	Sector		= 0;
+	Layer		= 0;
+	Component	= 0;
+	BarID		= 0;
+	Edep		= 0;
+	Tof		= 0;
+	TofFadc		= 0;
+	Tdiff		= 0;
+	TdiffFadc	= 0;
+	X		= 0;
+	Y		= 0;
+	Z		= 0;
+		
+	RawLtdc		= 0;
+	RawRtdc		= 0;
+	RawLtdccorr	= 0;
+	RawRtdccorr	= 0;
+	RawLtfadc	= 0;
+	RawRtfadc	= 0;
+	RawLamp		= 0;
+	RawRamp		= 0;
+	RawLadc		= 0;
+	RawRadc		= 0;
+		
+	PmtLtdc		= 0;
+	PmtRtdc		= 0;
+	PmtLtfadc	= 0;
+	PmtRtfadc	= 0;
+	PmtLamp		= 0;
+	PmtRamp		= 0;
+	PmtLadc		= 0;
+	PmtRadc		= 0;
+	PmtLped		= 0;
+	PmtRped		= 0;
+}
+//ClassImp(bandhit);
--- skimmers/readhipo_helper.cpp
+++ skimmers/readhipo_helper.cpp
@@ -1,44 +1,5 @@
 #include "readhipo_helper.h"
 
-bandhit::bandhit(){}
-
-void bandhit::Clear(){
-	Sector		= 0;
-	Layer		= 0;
-	Component	= 0;
-	BarID		= 0;
-	Edep		= 0;
-	Tof		= 0;
-	TofFadc		= 0;
-	Tdiff		= 0;
-	TdiffFadc	= 0;
-	X		= 0;
-	Y		= 0;
-	Z		= 0;
-		
-	RawLtdc		= 0;
-	RawRtdc		= 0;
-	RawLtdccorr	= 0;
-	RawRtdccorr	= 0;
-	RawLtfadc	= 0;
-	RawRtfadc	= 0;
-	RawLamp		= 0;
-	RawRamp		= 0;
-	RawLadc		= 0;
-	RawRadc		= 0;
-		
-	PmtLtdc		= 0;
-	PmtRtdc		= 0;
-	PmtLtfadc	= 0;
-	PmtRtfadc	= 0;
-	PmtLamp		= 0;
-	PmtRamp		= 0;
-	PmtLadc		= 0;
-	PmtRadc		= 0;
-	PmtLped		= 0;
-	PmtRped		= 0;
-}
-bandhit::~bandhit(){}
 
 double FADC_GLOBSHIFT[600] = {0.};
 double FADC_RUNBYRUNSHIFT[100000] = {0.};
